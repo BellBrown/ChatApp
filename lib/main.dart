dc3bdf563197efb5f0db1670aaa0a0d5e844a32b
BellBrown <94917218+BellBrown@users.noreply.github.com>
2021-12-02 10:03:11 +0000

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,12 +34,64 @@ class chatapp extends StatelessWidget {
          Icon(Icons.search),Icon(Icons.more), 
         ],
       ),
-      body: Center(child: CircleAvatar(
-        radius: 70,
-        backgroundColor: Colors.blue,
-        backgroundImage: NetworkImage("https://images.unsplash.com/photo-1638265437803-59b059b497f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"),
-        
-        ) ,
-    ));
+      body:Column(
+        children: [
+          ChatWidget(
+            name: "Ephuwah Brown",
+            message: "You are soo beautiful",
+            time: "2:00",
+            image: "https://images.unsplash.com/photo-1638431254808-57f4a950bf7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+          ),
+          ChatWidget(
+            name: "Ebo Brown",
+            message: "You are soo handsome",
+            time: "5:00",
+            image: "https://images.unsplash.com/photo-1638422379823-ce79ec2db5b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", 
+          ),
+          ChatWidget(
+            name: "Adjoa Tic",
+            message:"How is Edwin and the children",
+            time: "3:30",
+            image: "https://images.unsplash.com/photo-1638043881914-4423b7fa7210?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+          ),
+          ],
+      ) ,
+    );
+  }
+}
+
+class ChatWidget extends StatelessWidget {
+  const ChatWidget({
+    Key? key, required this.name, required this.message, required this.time, required this.image,
+  }) : super(key: key);
+  final String name;
+  final String message;
+  final String time;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      color: Colors.blue,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading:  CircleAvatar(
+            backgroundImage: NetworkImage(image),
+            radius: 30,
+          ),
+          title:  Text(name),
+          subtitle: Row(
+            children:  [
+             const Icon(Icons.done_all),
+              Text(message)
+            ],
+          ),
+          trailing: Text(time),
+        ),
+      ),
+
+    );
   }
 }
